42d7ad1af5c212365db0745b6812be4b667aa7a3
vasalikhitha <likhithavasa1077@gmail.com>
2021-06-06 20:13:37 +0530

    commit

---
+++ src/main/java/com/example/demo/SpringBootProjectDevopss1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectDevopss1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectDevopss1Application.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
